2149b27c97b0fe26eb10602daa88ae5c82ac6a96
Tyler Lundstrom <tlundstrom@worcester.edu>
2016-10-23 22:59:49 -0400

    Adding in value check and file writing

--- charterPing.py
+++ charterPing.py
@@ -14,18 +14,15 @@ def ping(host, numOfPings):
             now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
             if(ping[:2]=="64"):
                 return str(now) + " : " + str(ping)
-                time.sleep(1)
-
 
 file = open(time.strftime("%d-%m-%Y"), 'ab')
-
-location = ping("192.168.1.1", 1).find('time') + 5
-pingTime = ping("192.168.1.1", 1)[location:location+5]
-if(int(pingTime) > 0.500):
-    print pingTime
-
-#for num in range(0,300):
-    #file.write(str(ping("192.168.1.1", 1))+"\n")
-    #time.sleep(1)
+i = 0
+for num in range(0,300):
+    location = ping("192.168.1.1", 1).find('time') + 5
+    pingTime = ping("192.168.1.1", 1)[location:location+5]
+    if(float(pingTime) > 0.000):
+        file.write(str(ping("192.168.1.1", 1)) + " Ping: " + str(i) + " \n")
+    time.sleep(1)
+    i+=1
 
 file.close()
